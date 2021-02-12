eaa6d313888ab455182280edebf2c366c2044480
Hectahx <saeed1711@hotmail.co.uk>
2021-02-12 18:58:44 +0000

    Re added android app

---
+++ Android App/build/static/js/53.1a361930.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwho-was-in-paris"]=this["webpackJsonpwho-was-in-paris"]||[]).push([[53],{135:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return e}));var o=i(13),e=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,i=n.innerHeight,e=document.elementFromPoint(t/2,i/2);if(e){var s=e.closest("ion-content");s&&s.componentOnReady().then((function(){Object(o.f)((function(){return s.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.1a361930.chunk.js.map
