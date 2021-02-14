2b4608512e473c8422e34dbfd6db21198a023c5a
Hectahx <saeed1711@hotmail.co.uk>
2021-02-14 18:45:48 +0000

    hello world up again

--- programBytes/src/App.tsx
+++ programBytes/src/App.tsx
@@ -2,6 +2,8 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import NewItem from './pages/NewItem';
+import Gnomed from './pages/Gnomed';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -20,21 +22,21 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './theme/variables.css';
+//import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
-
+const App: React.FC = () => {
+  const isAuthed = true;
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/home" component={Home} />
+          <Route path="/new" component={NewItem} />
+          <Route path="/gnomed" component={Gnomed} />
+          <Redirect exact from="/" to="/home" />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+}
 export default App;
--- programBytes/src/pages/Home.tsx
+++ programBytes/src/pages/Home.tsx
@@ -1,25 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonIcon,IonFab, IonFabButton,IonButton } from '@ionic/react';
+import { add, basketball } from 'ionicons/icons';
+import { RouteComponentProps } from 'react-router-dom';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home: React.FC<RouteComponentProps> = (props) =>{
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Ionic Title</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonContent>
+    <IonList>
+      <IonItem>
+        <IonLabel>
+          <h1>Hello World</h1>
+        </IonLabel>
+      </IonItem>
+      <IonButton  expand="block" onClick={() => props.history.push('/gnomed')}>
+        <IonIcon icon={basketball} />
+        Click for google
+        <IonIcon icon={basketball} />
+      </IonButton>
+    </IonList>
+  <IonFab vertical="bottom" horizontal="end" slot="fixed">
+    <IonFabButton onClick={() => props.history.push('/new')}>
+      <IonIcon icon={add} />
+    </IonFabButton>
+  </IonFab>
+  </IonContent>
     </IonPage>
   );
 };
-
 export default Home;
